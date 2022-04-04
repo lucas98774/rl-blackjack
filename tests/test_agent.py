a49ea566fc47ee04e201c858f1aae139494fce4a
lucas <lucasspellman98@gmail.com>
2022-04-04 00:48:43 -0500

    create optional parameter

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,9 +1,8 @@
 from blackjack.environment import GameWAgents
 from blackjack.methods.MonteCarlo import MonteCarloExploringStarts as MCES
 
-
-def test_agent():
-    game = GameWAgents(MCES, nrounds=5)
+def test_agent(n_rounds=5):
+    game = GameWAgents(MCES, nrounds=n_rounds)
     try:
         game.play()
     except Exception as e:
